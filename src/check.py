a15ed50f6e12a535e517b88d196156a22b1f2835
souradip <souradipdey112@gmail.com>
2026-08-09 15:30:25 +0530

    update2

--- src/check.py
+++ src/check.py
@@ -3,3 +3,22 @@ def add(a,b):
 
 def dev(a,b):
     return a - b
+
+# find dup in the list
+def chek_dup(a:list ,b:list) -> list:
+
+    g = []
+
+    for i1 in a :
+        for i2 in b:
+            if i1 == i2:
+                g.append(i1)
+    print(g) 
+
+
+a = [1,2,3]
+b = [1,3,4]
+chek_dup(a,b)
+                
+
+
